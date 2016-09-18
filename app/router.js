3bd97e0b295bccadbea24979afd770db99995ffb
NatasaPeic <peic.natasa@gmail.com>
2016-09-17 20:45:40 -0400

    Deploy

---
+++ app/router.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+import config from './config/environment';
+
+const Router = Ember.Router.extend({
+  location: config.locationType,
+});
+
+Router.map(function () {
+  this.route('about');
+  this.route('team', function () {
+    this.route('engineering');
+    this.route('leadership');
+  });
+  this.route('contact', function(){
+    this.route('boston');
+    this.route('ny');
+  });
+});
+
+export default Router;
